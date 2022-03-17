a5d2e6ac52016dee14dc2dcc19a34ab663ebfc00
okAbrhem <fikadu.abdisa@gmail.com>
2022-03-16 21:52:03 -0700

    printing single digit numbers followed by new line character

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point for executing the code block
+ * a - varibale to store single digit numbers
+ * printf - used to print out strinn and integers to stdout
+ *
+ *
+ *Return: Always 0(Success)
+ */
+
+int main(void)
+{
+	int a;
+
+	while (a < 10)
+	{
+	printf("%d", a);
+	a++;
+	}
+	printf("\n");
+return (0);
+}
+
