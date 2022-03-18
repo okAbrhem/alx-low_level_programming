7e60a0046a9fc073330e4bfd6aef6f02881bce17
okAbrhem <fikadu.abdisa@gmail.com>
2022-03-17 22:29:30 -0700

    printing hexadecimals in C using a while loop

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -11,5 +11,23 @@
 
 int main(void)
 {
+	int var = 0;
+	char ch = 'a';
 
+	while (ch <= 'f')
+	{
+	while (var < 10)
+
+	{
+	putchar(var + '0');
+	var++;
+	}
+
+	putchar(ch);
+	ch++;
+	}
+
+	putchar('\n');
+	return (0);
+}
 
