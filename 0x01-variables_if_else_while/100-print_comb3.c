75b5b156089531ccc71a197f8594112ada976e04
okAbrhem <fikadu.abdisa@gmail.com>
2022-03-18 21:51:33 -0700

    printing all possible combinations of two-digit numbers in c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+
+/**
+ * main - entry for everything in c
+ * i - variable to store numbers
+ * j - variable to store numbers
+ * for - loop for iteration over some conditions
+ * putchar - function to print charatcters onto stdout
+ *
+ *
+ *
+ *Return: Alwways 0(Success)
+ */
+
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			if (i < j)
+			{
+				putchar(i + '0');
+				putchar(j + '0');
+				if (i != 8 || (i == 8 && j != 9))
+					putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
