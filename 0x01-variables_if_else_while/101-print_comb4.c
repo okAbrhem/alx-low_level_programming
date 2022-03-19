f7020538376dc360c6d2a6bce196ec5137faf728
okAbrhem <fikadu.abdisa@gmail.com>
2022-03-19 12:24:57 -0700

    printing all possible combinations of three-digit numbers in c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - entry point for everything
+ * putchar  - prints integers to the screen
+ * i,j,k - variables to store integers
+ * for - loop for interating over some condition
+ * if - condition to check for something
+ *
+ *
+ * return: Always 0(Success)
+ */
+
+int main(void)
+{
+	int i, j, k;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			for (k = 0; k <= 9; k++)
+			{
+				if ((i < j && i < k) && (j < k))
+				{
+					putchar(i + '0');
+					putchar(j + '0');
+					putchar(k + '0');
+					if ((i != 8 && k != 9) || (i == 8 && j != 9) || (i != 7))
+						putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+		putchar('\n');
+		return (0);
+}
