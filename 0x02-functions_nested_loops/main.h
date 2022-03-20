bb3523bb546346f616388be6fc43bd15c7a653d4
okAbrhem <fikadu.abdisa@gmail.com>
2022-03-20 15:15:39 -0700

    header file for _putchar function prototype

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+
+#define MAIN_H
+/*
+ *
+ * File: main.h
+ *
+ * Auth: Fikadu Abdisa
+ *
+ * Desc: Header file containing declarations for all functions
+ *
+ * used in the 0x02-functions_nested_loops directory.
+ *
+ */
+
+int _putchar(char c);
