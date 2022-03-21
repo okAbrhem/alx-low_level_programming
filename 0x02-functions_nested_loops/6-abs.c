968b234f685dbc94bc7d3224bb7c0a85cf3ffaf9
okAbrhem <fikadu.abdisa@gmail.com>
2022-03-21 09:40:34 -0700

    updated code for absolute value of an integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _abs - checks the absolute value of a number
+ * @j: integer to be checked
+ * if - conditional statement for the varibales to be checked
+ *
+ *
+ *Return: j if poistive, otherwise -j
+ */
+
+int _abs(int j)
+{
+
+if (j >= 0)
+{
+	return (j);
+}
+else
+{
+return (-j);
+}
+}
