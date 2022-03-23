ea01db667d75533868bf1adee7ae78a5795513cf
okAbrhem <fikadu.abdisa@gmail.com>
2022-03-22 21:29:54 -0700

    updated comment and code for adding two integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - return the sum of two integers
+ * @a: variable to store integers
+ * @b: variable to store integers
+ *
+ * Return: sum of tow integers
+ */
+int add(int a, int b)
+{
+int sum;
+sum = a + b;
+return (sum);
+}
