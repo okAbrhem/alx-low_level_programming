2320c534d621e927d5d65911078e117431bb873c
okAbrhem <fikadu.abdisa@gmail.com>
2022-04-11 11:35:20 -0700

    defining a macro named SIZE

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,12 @@
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
+
+/**
+ * SIZE - name of the macro
+ * desc: defines a macro named SIZE as an abbreviation
+ * for token 1024
+ */
+
+#define SIZE 1024
+
+#endif
---
+++ 0x0D-preprocessor/README.md
@@ -0,0 +1,1 @@
+This is project focuses on preprocessor!
