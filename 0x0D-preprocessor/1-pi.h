07310bed96903bbf496dd49dfc6e733d2239da40
okAbrhem <fikadu.abdisa@gmail.com>
2022-04-11 11:45:11 -0700

    printing name of the file to be compiled

---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,12 @@
+#ifndef PI_H
+#define PI_H
+
+/**
+ * PI - macro for consatnt number pi
+ * desc: PI is a macro abbreviation for 3.14159265359
+ *
+ */
+
+#define PI 3.14159265359
+
+#endif
---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of the code block
+ * printf - prints to stdout
+ *
+ * Return: 0 success
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
