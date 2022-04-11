dd2dcb423075cd1afbbdc14682a0cb34ac23a0b5
okAbrhem <fikadu.abdisa@gmail.com>
2022-04-11 12:16:12 -0700

    macro for absolute value of x

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,10 @@
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+
+/**
+ * ABS - calculates absolute value of x
+ * @x: value to be checked
+ */
+
+#define ABS(x) ((x) < 0 ? -(x) : (x))
+#endif
