14042c65b29ac0d11afeff8bbf9df06cc37f097f
okAbrhem <fikadu.abdisa@gmail.com>
2022-04-21 14:51:02 -0700

    printing something before main function is executed

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * myStartupFun - prints something before main funcion is executed!
+ *
+ * Return: nothing
+ */
+void myStartupFun(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
